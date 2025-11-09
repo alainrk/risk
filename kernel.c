e6cba8694c05378afe94aceef3e643a54933bed7
Alain <alain.devcs@gmail.com>
2025-11-09 05:50:45 +0100

    doc and build

--- kernel.c
+++ kernel.c
@@ -3,6 +3,8 @@ typedef unsigned int uint32_t;
 typedef uint32_t size_t;
 
 // References to the symbols created in kernel.ld
+// The "[]" is used as we need the start address of these values, omitting it
+// will give us the value at the 0th byte of that section.
 extern char __bss[], __bss_end[], __stack_top[];
 
 void *memset(void *buf, char c, size_t n) {
@@ -13,15 +15,23 @@ void *memset(void *buf, char c, size_t n) {
 }
 
 void kernel_main(void) {
+  // Sometimes the bss section is initialized at 0 by the bootloader, but it's
+  // better to make sure it's done.
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
-
+  // Infinite kernel loop.
   for (;;)
     ;
 }
 
-// As specified in kerel.ld boot() is the entry point of the kernel after boot
+// As specified in kerel.ld boot() is the entry point of the kernel after boot.
+// __volatile__ avoids the compiler optimizing the function.
+// Attributes:
+//    - section(".text.boot"): placement of the function in the linker script
+//    (0x80200000, where OpenSBI will jump to, without knowing anything the
+//    entry point)
+//    - naked: asks the compiler not to generate any unnecessary code before and
+//    after the asm inline function body (e.g. return)
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
-  // __volatile__ avoids the compiler optimizing this stuff
   __asm__ __volatile__(
       "mv sp, %[stack_top]\n" // Set the stack pointer to the end of the stack
                               // area (grows backwards to 0).
--- run.sh
+++ run.sh
@@ -4,6 +4,21 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+# Ubuntu users: use CC=clang
+CC=/opt/homebrew/opt/llvm/bin/clang
+
+# Build the kernel
+#   -g3 Generate the maximum amount of debug information.
+#   --target=riscv32-unknown-elf Compile for 32-bit RISC-V.
+#   -ffreestanding Do not use the standard library of the host environment (your development environment).
+#   -fuse-ld=lld Use LLVM linker
+#   -fno-stack-protector Disable unnecessary stack protection to avoid unexpected behavior in stack manipulation.
+#   -nostdlib Do not link the standard library.
+#   -Wl,-Tkernel.ld Specify the linker script.
+#   -Wl,-Map=kernel.map Output a map file (linker allocation result).
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
 # Start QEMU:
 #   -machine virt       Start a virt machine.
 #                       Check other supported machines with the -machine '?' option.
@@ -12,4 +27,9 @@ QEMU=qemu-system-riscv32
 #   -serial mon:stdio   Connect QEMU's standard i/o to the virtual machine's serial port.
 #                       Specifying `mon:` allows switching to the QEMU monitor by pressing Ctrl+A then C.
 #   --no-reboot         If the virtual machine crashes, stop the emulator without rebooting (useful for debugging).
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt \
+	-bios default \
+	-nographic \
+	-serial mon:stdio \
+	--no-reboot \
+	-kernel kernel.elf
