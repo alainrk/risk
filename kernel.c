2594ac7044a6515211cc8302fb7a21b1f26356f7
Alain <alain.devcs@gmail.com>
2025-11-15 06:04:21 +0100

    exception handler

--- kernel.c
+++ kernel.c
@@ -37,3 +37,99 @@ __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
           __stack_top) // Pass the stack top address as %[stack_top]
   );
 }
+
+// Exception handler.
+// Entry point of the exception handler to be stores in stvec (Supervisor Trap
+// Vector Base Address Register).
+//
+// - sscratch is used as a temporary storage to save the stack pointer at the
+// time of exception occurrence, which is later restored.
+// - Floating-point registers are not used within the kernel, and thus there's
+// no need to save them here. Generally, they are saved and restored during
+// thread switching.
+// - The stack pointer is set in the a0 register, and the handle_trap function
+// is called. SP now contains register values stored in the same structure as
+// the trap_frame.
+// - __attribute__((aligned(4))) aligns the function's starting address to a
+// 4-byte boundary, since stvec not only holds the address of the exception
+// handler but also has flags representing the mode in its lower 2 bits.
+//
+// NOTE: The entry point of exception handlers is critical and error-prone.
+// The original values of general-purpose registers are saved onto the stack,
+// even SP by using sscratch. Accidentally overwriting the a0 register can lead
+// to hard-to-debug problems like "local variable values change for no apparent
+// reason". Saving the program state correctly is critical!
+// ----------------------------------------------------------------------------
+__attribute__((naked)) __attribute__((aligned(4))) void kernel_entry(void) {
+  __asm__ __volatile__("csrw sscratch, sp\n"
+                       "addi sp, sp, -4 * 31\n"
+                       "sw ra,  4 * 0(sp)\n"
+                       "sw gp,  4 * 1(sp)\n"
+                       "sw tp,  4 * 2(sp)\n"
+                       "sw t0,  4 * 3(sp)\n"
+                       "sw t1,  4 * 4(sp)\n"
+                       "sw t2,  4 * 5(sp)\n"
+                       "sw t3,  4 * 6(sp)\n"
+                       "sw t4,  4 * 7(sp)\n"
+                       "sw t5,  4 * 8(sp)\n"
+                       "sw t6,  4 * 9(sp)\n"
+                       "sw a0,  4 * 10(sp)\n"
+                       "sw a1,  4 * 11(sp)\n"
+                       "sw a2,  4 * 12(sp)\n"
+                       "sw a3,  4 * 13(sp)\n"
+                       "sw a4,  4 * 14(sp)\n"
+                       "sw a5,  4 * 15(sp)\n"
+                       "sw a6,  4 * 16(sp)\n"
+                       "sw a7,  4 * 17(sp)\n"
+                       "sw s0,  4 * 18(sp)\n"
+                       "sw s1,  4 * 19(sp)\n"
+                       "sw s2,  4 * 20(sp)\n"
+                       "sw s3,  4 * 21(sp)\n"
+                       "sw s4,  4 * 22(sp)\n"
+                       "sw s5,  4 * 23(sp)\n"
+                       "sw s6,  4 * 24(sp)\n"
+                       "sw s7,  4 * 25(sp)\n"
+                       "sw s8,  4 * 26(sp)\n"
+                       "sw s9,  4 * 27(sp)\n"
+                       "sw s10, 4 * 28(sp)\n"
+                       "sw s11, 4 * 29(sp)\n"
+
+                       "csrr a0, sscratch\n"
+                       "sw a0, 4 * 30(sp)\n"
+
+                       "mv a0, sp\n"
+                       "call handle_trap\n"
+
+                       "lw ra,  4 * 0(sp)\n"
+                       "lw gp,  4 * 1(sp)\n"
+                       "lw tp,  4 * 2(sp)\n"
+                       "lw t0,  4 * 3(sp)\n"
+                       "lw t1,  4 * 4(sp)\n"
+                       "lw t2,  4 * 5(sp)\n"
+                       "lw t3,  4 * 6(sp)\n"
+                       "lw t4,  4 * 7(sp)\n"
+                       "lw t5,  4 * 8(sp)\n"
+                       "lw t6,  4 * 9(sp)\n"
+                       "lw a0,  4 * 10(sp)\n"
+                       "lw a1,  4 * 11(sp)\n"
+                       "lw a2,  4 * 12(sp)\n"
+                       "lw a3,  4 * 13(sp)\n"
+                       "lw a4,  4 * 14(sp)\n"
+                       "lw a5,  4 * 15(sp)\n"
+                       "lw a6,  4 * 16(sp)\n"
+                       "lw a7,  4 * 17(sp)\n"
+                       "lw s0,  4 * 18(sp)\n"
+                       "lw s1,  4 * 19(sp)\n"
+                       "lw s2,  4 * 20(sp)\n"
+                       "lw s3,  4 * 21(sp)\n"
+                       "lw s4,  4 * 22(sp)\n"
+                       "lw s5,  4 * 23(sp)\n"
+                       "lw s6,  4 * 24(sp)\n"
+                       "lw s7,  4 * 25(sp)\n"
+                       "lw s8,  4 * 26(sp)\n"
+                       "lw s9,  4 * 27(sp)\n"
+                       "lw s10, 4 * 28(sp)\n"
+                       "lw s11, 4 * 29(sp)\n"
+                       "lw sp,  4 * 30(sp)\n"
+                       "sret\n");
+}
--- kernel.h
+++ kernel.h
@@ -1,1 +1,11 @@
 #pragma once
+
+// Note "##" removes the preceding "," where there's only 1 arg.
+// __VA_ARGS__ is supported by the compiler extension for macros with
+// variable number of args.
+#define PANIC(fmt, ...)                                                        \
+  do {                                                                         \
+    printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);      \
+    while (1) {                                                                \
+    }                                                                          \
+  } while (0)
